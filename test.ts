77e38d74db735d31e2a89723b7e0bff040a96368
mehdi havaei <havaei@imenaria.org>
2022-02-13 21:17:22 +0330

    first  commit

---
+++ test.ts
@@ -0,0 +1,104 @@
+const x: number = 2;
+
+let user: string;
+
+user = "lorem"
+
+let temp: number = 33;
+
+temp = 24
+
+let check: Boolean;
+
+check = true
+
+///////////////////
+
+let newObject: {
+    name: string,
+    age: number
+};
+
+newObject = {
+    name: "peter",
+    age: 24
+};
+
+let testString: string[];
+
+testString = ["jack","jim", "22"]
+
+let testNumber: number[];
+
+testNumber = [1,2,3,4]
+
+///////////////////////////
+
+let advance: {
+    name: string,
+    age: number
+}[];
+
+advance = [
+    {
+        name:"mmm",
+        age: 22
+    },
+    {
+        name:"sss",
+        age: 33
+    },
+]
+
+///////////////
+//! type inference
+
+let temp7 = "React - TypeScript";
+
+temp7 = "Hello World";
+
+///////////////////////////////////
+//! Union type
+
+let union: string | number;
+
+union = "bye bye";
+union = 12;
+
+/////////////////////////
+//! Aliases type
+type myType = {
+    name: string,
+    age: number,
+    isActive: boolean
+}
+
+let ggg: myType;
+
+ggg = {
+    name: "name",
+    age: 33,
+    isActive: true
+}
+
+///////////////////
+
+function add(a: number, b:number) : number | string {
+    return a + b;
+}
+
+//////////////////
+//! Generics
+
+// function generic(array: any[], value: any) {
+//     const newArray = [...array, value];
+//     return newArray; 
+// }
+function generic<T>(array: T[], value: T) {
+    const newArray = [...array, value];
+    return newArray; 
+}
+
+const demoArray = [1, 2, 3];
+const test11 = generic(demoArray, -1); // [1,2,3,-1]
+const test22 = generic(["1","2","3"], "-1");
